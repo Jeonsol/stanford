448d5a9c3e5abce37dae2357b7427bf249d06082
전솔 <jeonsol12@naver.com>
2018-07-20 00:39:29 +0900

    [계산기] 계산기를 만들어 보자6 - 연산자 pi를 추가해보자

--- stanford/ViewController.swift
+++ stanford/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var displayNumber: String?
+    var isMath: Bool = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        if isMath {
+            displayNumber = display.text!+(sender.titleLabel?.text!)!
+            display.text = displayNumber
+        }
+        else {
+            display.text = sender.titleLabel?.text!
+        }
+        isMath = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func calFunc(_ sender: UIButton) {
+        if let displayText = sender.titleLabel?.text {
+            if displayText == "pi" {
+                display.text = String(Double.pi)
+            }
+        }
     }
-
-
 }
 
